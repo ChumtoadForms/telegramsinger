f56904ec6ba4a3209ab94de113463173b1a6d9ad
rogender <89637241+rogender@users.noreply.github.com>
2022-02-10 21:23:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+# СЛЕДУЮЩИЕ ДВЕ СТРОЧКИ НЕ МЕНЯТЬ, В ПРОТИВНОМ СЛУЧАЕ ВСЁ СЛОМАЕТСЯ
 api_id = 18539672
 api_hash = 'c9cee400e328a55aacec41f7d19d8743'
+
 songtext = open('song.txt', encoding='utf-8')
+delaytime = 80
